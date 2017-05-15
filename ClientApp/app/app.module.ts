316099bb3d239825b330c9d3aab3dd32234d4e14
Patrick Gomes <patrickgomes@gmail.com>
2017-05-15 02:46:15 -0500

    Added Vehicle API incl Resources for Vehicles and Contacts but its not working

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,12 +1,15 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
+
 import { AppComponent } from './components/app/app.component'
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
+import { VehicleService } from "./services/vehicle.service";
 
 @NgModule({
     bootstrap: [ AppComponent ],
@@ -18,16 +21,20 @@ import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.com
         HomeComponent,
         VehicleFormComponent
     ],
-    imports: [
+    imports: [   
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
+        FormsModule, 
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'vehicles\new', component: VehicleFormComponent },
+            { path: 'vehicles/new', component: VehicleFormComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
-        ])
+        ],)
+    ],
+    providers: [
+        VehicleService,
     ]
 })
 export class AppModule {
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using System.Linq;
 using vega.Controllers.Resources;
 using vega.Models;
 
@@ -8,14 +9,21 @@ namespace vega.Mapping
     {
         public MappingProfile()
         {
+            // Domain to API Resource
             CreateMap<Make, MakeResource>();
-            CreateMap<MakeResource, Make>();
-
             CreateMap<Model, ModelResource>();
-            CreateMap<ModelResource, Model>();
-
             CreateMap<Feature, FeatureResource>();
-            CreateMap<FeatureResource, Feature>();
+            CreateMap<Vehicle, VehicleResource>()
+              .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource { Name = v.ContactName, Email = v.ContactEmail, Phone = v.ContactPhone } ))
+              .ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => vf.FeatureId)));
+
+            // API Resource to Domain
+            CreateMap<VehicleResource, Vehicle>()
+              .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
+              .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
+              .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
+              .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature { FeatureId = id })));
+
         }
     }
 }
